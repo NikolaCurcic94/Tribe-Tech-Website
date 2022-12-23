9bd5860f587b74adf03eebebeed659d6c3fe01e4
Nikola Curcic <nikolacurcic94gmail.com>
2022-12-23 17:27:37 +0100

    gg

--- js/index.js
+++ js/index.js
@@ -22,20 +22,25 @@
 
 const menuButton = document.querySelector(".menu__button");
 const closeButton = document.querySelector(".close__button");
-const navLinks = document.querySelector(".nav__links");
-const navLink = document.querySelector(".nav__link");
+//const navLinks = document.querySelector(".nav__links");
+const navLink = document.querySelectorAll(".nav__link");
 const aboutLink = document.querySelector(".about__link");
 
 menuButton.addEventListener("click", () => {
-  navLinks.classList.add("nav__links--open");
+  //navLinks.classList.add("nav__links--open");
   closeButton.classList.remove("invisible");
   menuButton.classList.add("invisible");
-  navLink.classList.add("visible");
-  navLink.classList.remove("hidden");
+  // navLink.classList.add("visible");
+  // navLink.classList.remove("hidden");
+  console.log(navLink);
+  navLink.forEach((element) => {
+    element.classList.add("visible");
+    element.classList.remove("hidden");
+  });
 });
 
 closeButton.addEventListener("click", () => {
-  navLinks.classList.remove("nav__links--open");
+  //navLinks.classList.remove("nav__links--open");
   closeButton.classList.add("invisible");
   menuButton.classList.remove("invisible");
   navLink.classList.remove("visible");
