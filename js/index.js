e25b14257096c4507e92c86939b620b4cfb74955
Aleksandar Randjelovic <arandjelovic@showingtime.com>
2022-12-23 18:04:20 +0100

    Sale commit

--- js/index.js
+++ js/index.js
@@ -20,35 +20,38 @@
 
 /*****************************Menu button**********************************/
 
-const menuButton = document.querySelector(".menu__button");
-const closeButton = document.querySelector(".close__button");
-const navLinks = document.querySelector(".nav__links");
-const navLink = document.querySelectorAll(".nav__link");
-const aboutLink = document.querySelector(".about__link");
+document.addEventListener('DOMContentLoaded', () => {
 
-menuButton.addEventListener("click", () => {
-  navLinks.classList.add("nav__links--open");
-  closeButton.classList.remove("invisible");
-  menuButton.classList.add("invisible");
-  navLink.classList.add("visible");
-  navLink.classList.remove("hidden");
-  console.log(navLink);
-  navLink.forEach((element) => {
-    element.classList.add("visible");
-    element.classList.remove("hidden");
+  const menuButton = document.querySelector(".menu__button");
+  const closeButton = document.querySelector(".close__button");
+  const navLinks = document.querySelector(".nav__links");
+  const navLink = document.querySelectorAll(".nav__link");
+  const aboutLink = document.querySelector(".about__link");
+
+  menuButton.addEventListener("click", () => {
+    navLinks.classList.add("nav__links--open");
+    closeButton.classList.remove("invisible");
+    menuButton.classList.add("invisible");
+    console.log(document.querySelectorAll(".nav__link"));
+    document.querySelectorAll(".nav__link").forEach((element) => {
+      element.classList.remove("hidden");
+      element.classList.add("visible");
+    });
   });
+
+  closeButton.addEventListener("click", () => {
+    navLinks.classList.remove("nav__links--open");
+    closeButton.classList.add("invisible");
+    menuButton.classList.remove("invisible");
+    navLink.classList.remove("visible");
+    navLink.classList.add("hidden");
+  });
+
+  aboutLink.addEventListener("click", () => {
+    navLinks.classList.remove("nav__links--open");
+    closeButton.classList.add("invisible");
+    menuButton.classList.remove("invisible");
+  });
+
 });
 
-closeButton.addEventListener("click", () => {
-  navLinks.classList.remove("nav__links--open");
-  closeButton.classList.add("invisible");
-  menuButton.classList.remove("invisible");
-  navLink.classList.remove("visible");
-  navLink.classList.add("hidden");
-});
-
-aboutLink.addEventListener("click", () => {
-  navLinks.classList.remove("nav__links--open");
-  closeButton.classList.add("invisible");
-  menuButton.classList.remove("invisible");
-});
